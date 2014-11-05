b1abd35e299b0ee75f1b86e346ec3fe98ff713c3
monkut <shane.cousins@gmail.com>
2014-11-05 10:31:22 +0900

    initial setup file

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.1',
+      description='Utilities for Creating TMS tiles',
+      author='Shane Cousins',
+      author_email='shane.cousins@gmail.com',
+      url='https://github.com/monkut/tmstiles',
+      packages=['tmstiler',],
+     )
